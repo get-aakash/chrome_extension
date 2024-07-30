3f6a70c5b64d44f5d0d28e4ed1008ad4978a08db
Aakash Acharya <mail.aakash108@gmail.com>
2024-07-30 23:41:00 +1000

    add event listener

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+let inputBtn = document.getElementById("input-btn")
+let boxItem = document.getElementById("box")
+
+
+inputBtn.addEventListener("click", function(){
+    console.log("Button clicked from add event listener!")
+})
+
+boxItem.addEventListener("click", function(){
+    console.log("I want to open the box!")
+})
