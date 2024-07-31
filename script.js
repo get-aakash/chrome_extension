10ea6f8745e901a7da2ea72de8e688fdf02b65d4
Aakash Acharya <mail.aakash108@gmail.com>
2024-07-31 23:17:32 +1000

    unordered list

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 
-let myLeads = []
+let myLeads = ["a","b","c","d"]
 
 let inputEl = document.getElementById("input-el")
 
 let inputBtn = document.getElementById("input-btn")
 
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
 
+for(let i= 0; i<myLeads.length;i++){
+    ulEl.textContent += myLeads[i] 
+}
