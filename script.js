60093c0aa8e33d973d51f459f9e008f926f31085
Aakash Acharya <mail.aakash108@gmail.com>
2024-07-31 23:23:36 +1000

    innerhtml

--- script.js
+++ script.js
@@ -7,11 +7,15 @@ let inputBtn = document.getElementById("input-btn")
 
 const ulEl = document.getElementById("ul-el")
 
+const containerEl = document.getElementById("container-el")
+
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
 
 for(let i= 0; i<myLeads.length;i++){
-    ulEl.textContent += myLeads[i] 
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li" 
 }
+
+containerEl.innerHTML = "<button>" + "Buy!" + "</button>"
