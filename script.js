ebd8a7c3a2dfd19d3ce4e2905e183f1a5f1fe745
Aakash Acharya <mail.aakash108@gmail.com>
2024-08-03 01:09:40 +1000

    preparing for deployment

--- script.js
+++ script.js
@@ -12,8 +12,9 @@ const containerEl = document.getElementById("container-el")
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
-
+    inputEl.value = ""
     renderLeads()
+    
 })
 
 
@@ -24,9 +25,11 @@ function renderLeads() {
     for (let i = 0; i < myLeads.length; i++) {
 
         listitems += `<li>
-        
-        <a target='_blank' href='${myLeads[i]}' >${myLeads[i]}</a></li>`
+        <a target='_blank' href='${myLeads[i]}'>
+            ${myLeads[i]}
+        </a>
+                </li>`
     }
 
-    ulEl.innerHTML =  listitems 
+    ulEl.innerHTML = listitems
 }
