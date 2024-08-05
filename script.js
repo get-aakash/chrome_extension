9c8a650d7f0e66eeadf63d38999b49d0d9d45a2a
Aakash Acharya <mail.aakash108@gmail.com>
2024-08-05 19:24:28 +1000

    local storage

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 let myLeads = []
 
+
 let inputEl = document.getElementById("input-el")
 
 let inputBtn = document.getElementById("input-btn")
@@ -9,12 +10,15 @@ const ulEl = document.getElementById("ul-el")
 
 const containerEl = document.getElementById("container-el")
 
-
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
     
+    console.log()
 })
 
 
